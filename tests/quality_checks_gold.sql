23ec6d380e218e4aa889f54e15483b1f33a7bdc5
aman-dhakad-data <amandhakad258000@gmail.com>
2026-05-13 18:37:55 +0530

    Update and rename placeholder to quality_checks_gold.sql

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,74 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.dim_products'
+-- ====================================================================
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results 
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- Check for NULLs in product_key
+-- Expectation: No results
+SELECT *
+FROM gold.dim_products
+WHERE product_key IS NULL;
+
+-- ====================================================================
+-- Checking 'gold.fact_sales'
+-- ====================================================================
+-- Check the data model connectivity between fact and dimensions
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL -- Missing product references
+  OR c.customer_key IS NULL -- Missing customer references
+
+-- Negative or zero sales
+SELECT *
+FROM gold.fact_sales
+WHERE sales_amount <= 0;
+
+-- Quantity should be positive
+SELECT *
+FROM gold.fact_sales
+WHERE quantity <= 0;
+
+-- Shipping date before order date
+SELECT *
+FROM gold.fact_sales
+WHERE shipping_date < order_date;
+
